4b6edfbe1d757ea91598a303aedc6cc73cf733da
Moody Salem <moody@fastmodeltechnologies.com>
2015-07-20 10:35:59 -0500

    Fix the submit function of the form

--- src/main/resources/META-INF/resources/rbs/components/model/Form.js
+++ src/main/resources/META-INF/resources/rbs/components/model/Form.js
@@ -2,58 +2,60 @@
  * Renders a model's attributes into a form
  */
 define([ "react", "underscore", "../mixins/Model", "../mixins/FormGroup" ],
-    function (React, _, model, formGroup) {
-        "use strict";
+  function (React, _, model, formGroup) {
+    "use strict";
 
-        return _.rf({
-            displayName: "Model Form",
+    return _.rf({
+      displayName: "Model Form",
 
-            mixins: [ model, formGroup ],
+      mixins: [ model, formGroup ],
 
-            getInitialState: function () {
-                return {
-                    submitting: false
-                };
-            },
+      getInitialState: function () {
+        return {
+          submitting: false
+        };
+      },
 
-            render: function () {
-                var children = _.map(this.getAttributes(this.props.attributes), this.makeFormGroup);
+      render: function () {
+        var children = _.map(this.getAttributes(this.props.attributes), this.makeFormGroup);
 
-                if (this.state.submitting) {
-                    children.push(React.DOM.input({
-                        type: "submit",
-                        ref: "_tempSubmitBtn"
-                    }));
-                }
+        if (this.state.submitting) {
+          children.push(React.DOM.input({
+            type: "submit",
+            ref: "_tempSubmitBtn",
+            key: "_tsb",
+            style: { display: "none" }
+          }));
+        }
 
-                return React.DOM.form(_.extend({}, this.props, {
-                    onSubmit: this.beforeSubmit
-                }), children);
-            },
+        return React.DOM.form(_.extend({}, this.props, {
+          onSubmit: this.beforeSubmit
+        }), children);
+      },
 
-            beforeSubmit: function (e) {
-                e.preventDefault();
-                if (typeof this.props.onSubmit === "function") {
-                    this.props.onSubmit(e);
-                }
-            },
+      beforeSubmit: function (e) {
+        e.preventDefault();
+        if (typeof this.props.onSubmit === "function") {
+          this.props.onSubmit(e);
+        }
+      },
 
-            submit: function () {
-                if (this.isMounted()) {
-                    if (this.state.submitting === false) {
-                        this.setState({
-                            submitting: true
-                        }, function () {
-                            if (this.isMounted()) {
-                                var btn = React.findDOMNode(this.refs._tempSubmitBtn);
-                                btn.click();
-                                this.setState({
-                                    submitting: false
-                                });
-                            }
-                        });
-                    }
-                }
-            }
-        });
+      submit: function () {
+        if (this.isMounted()) {
+          if (this.state.submitting === false) {
+            this.setState({
+              submitting: true
+            }, function () {
+              if (this.isMounted()) {
+                var btn = React.findDOMNode(this.refs._tempSubmitBtn);
+                btn.click();
+                this.setState({
+                  submitting: false
+                });
+              }
+            });
+          }
+        }
+      }
     });
+  });
