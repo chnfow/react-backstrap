56a1e529a2991826444c232594f2e4f9a23a273a
Moody Salem <moody@fastmodeltechnologies.com>
2015-07-30 12:55:52 -0500

    Better table

--- src/main/resources/META-INF/resources/rbs/components/collection/Table.js
+++ src/main/resources/META-INF/resources/rbs/components/collection/Table.js
@@ -14,13 +14,19 @@ define([ "react", "underscore", "../mixins/Collection" ], function (React, _, co
     propTypes: {
       columns: rpt.arrayOf(rpt.shape({ label: rpt.string.isRequired, sortOn: rpt.string })).isRequired,
       striped: rpt.bool,
-      condensed: rpt.bool
+      condensed: rpt.bool,
+      hover: rpt.bool,
+      bordered: rpt.bool,
+      responsive: rpt.bool
     },
 
     getDefaultProps: function () {
       return {
-        striped: false,
-        condensed: false
+        striped: true,
+        condensed: false,
+        hover: false,
+        bordered: false,
+        responsive: true
       };
     },
     getHeader: function () {
@@ -45,7 +51,7 @@ define([ "react", "underscore", "../mixins/Collection" ], function (React, _, co
       return React.DOM.tbody({ key: "tbody" }, this.getModels());
     },
 
-    render: function () {
+    getTable: function () {
       var cn = [ "table" ];
       if (this.props.striped) {
         cn.push("table-striped");
@@ -53,10 +59,22 @@ define([ "react", "underscore", "../mixins/Collection" ], function (React, _, co
       if (this.props.condensed) {
         cn.push("table-condensed");
       }
+      if (this.props.hover) {
+        cn.push("table-hover");
+      }
+
       return React.DOM.table(_.extend(_.omit(this.props, "columns"), { className: cn.join(" ") }), [
         this.getHeader(),
         this.getBody()
       ]);
+    },
+
+    render: function () {
+      var table = this.getTable();
+      if (this.props.responsive) {
+        return React.DOM.div({ className: "table-responsive" }, table);
+      }
+      return table;
     }
   });
 });
