179c645231b3ca2363812a6d6da8ec9efc6eb470
Moody Salem <moody@fastmodeltechnologies.com>
2015-08-14 13:08:57 -0500

    Builds minified JS files now

---
+++ src/main/config/buildconfig.js
@@ -0,0 +1,9 @@
+({
+  // where the built files go
+  dir: "${project.build.directory}/classes/META-INF/resources/rbs",
+  // where the optimizer looks for css/js files
+  appDir: "${basedir}/src/main/resources/META-INF/resources/rbs",
+  // the assumed base URL for the require
+  baseUrl: "",
+  modules: []
+})
