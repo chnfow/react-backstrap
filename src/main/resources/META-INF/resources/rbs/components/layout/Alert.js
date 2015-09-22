ad8eeed14ce33c79bfe2a93543d3d10a01ddd09c
Moody Salem <moody@fastmodeltechnologies.com>
2015-09-22 10:08:46 -0500

    Swapping layout-alert hierarchy, layout should never depend on model components

--- src/main/resources/META-INF/resources/rbs/components/layout/Alert.js
+++ src/main/resources/META-INF/resources/rbs/components/layout/Alert.js
@@ -1,7 +1,7 @@
 /**
- * React Component
+ * Renders a bootstrap alert
  */
-define([ "react", "underscore", "backbone", "../model/Alert" ], function (React, _, Backbone, alert) {
+define([ "react", "underscore", "./Icon" ], function (React, _, icon) {
   "use strict";
 
   return _.rf({
@@ -10,19 +10,39 @@ define([ "react", "underscore", "backbone", "../model/Alert" ], function (React,
     mixins: [ React.addons.PureRenderMixin ],
 
     propTypes: {
-      level: React.PropTypes.oneOf([ "success", "info", "danger", "warning" ]),
+      level: React.PropTypes.oneOf([ "success", "info", "danger", "warning" ]).isRequired,
       strong: React.PropTypes.node.isRequired,
       message: React.PropTypes.node.isRequired,
-      icon: React.PropTypes.string
+      icon: React.PropTypes.oneOfType([ React.PropTypes.node, React.PropTypes.string ])
     },
 
     getDefaultProps: function () {
       return {};
     },
 
+    getClass: function () {
+      var classes = [ "alert" ];
+      if (typeof this.props.className === "string") {
+        classes.push(this.props.className);
+      }
+      classes.push("alert-" + this.props.level);
+      return classes.join(" ");
+    },
+
+    getIcon: function () {
+      var ic = this.props.icon;
+      if (typeof ic === "string") {
+        ic = icon({ key: "icon", name: ic });
+      }
+      return ic;
+    },
+
     render: function () {
-      var tempModel = new Backbone.Model(this.props);
-      return alert(_.extend({}, this.props, { model: tempModel }));
+      return React.DOM.div(_.extend({}, this.props, { className: this.getClass(), role: "alert" }), [
+        this.getIcon(),
+        React.DOM.strong({ key: "strong" }, this.props.strong),
+        this.props.message
+      ]);
     }
   });
 });
--- src/main/resources/META-INF/resources/rbs/components/model/Alert.js
+++ src/main/resources/META-INF/resources/rbs/components/model/Alert.js
@@ -1,55 +1,16 @@
 /**
  * Renders a model representing an alert into a bootstrap alert
  */
-define([ "react", "underscore", "../mixins/Model" ], function (React, _, model) {
+define([ "react", "underscore", "../mixins/Model", "../layout/Alert" ], function (React, _, model, alert) {
   "use strict";
-
-  var defaultAttributes = [
-    {
-      key: "icon",
-      attribute: "icon",
-      component: "icon"
-    },
-    {
-      key: "strong",
-      attribute: "strong",
-      component: React.DOM.strong
-    },
-    {
-      key: "message",
-      attribute: "message",
-      component: React.DOM.span
-    }
-  ];
 
   return _.rf({
     displayName: "Model Alert",
 
-    getDefaultProps: function () {
-      return {
-        attributes: defaultAttributes
-      };
-    },
-
     mixins: [ model, React.addons.PureRenderMixin ],
 
     render: function () {
-      var cn = [ "alert" ];
-
-      if (this.state.model.level) {
-        cn.push("alert-" + this.state.model.level);
-      } else {
-        cn.push("alert-info");
-      }
-
-      if (typeof this.props.className === "string") {
-        cn.push(this.props.className);
-      }
-
-      return React.DOM.div(_.extend({}, this.props, {
-        className: cn.join(" "),
-        role: "alert"
-      }), this.getAttributes(this.props.attributes));
+      return alert(this.state.model);
     }
   });
 });
