193b12a10ced864690217fcfda48f643035a2493
Moody Salem <moody@fastmodeltechnologies.com>
2015-10-22 12:57:15 -0500

    Improvements on mobile display of the select results

--- src/main/resources/META-INF/resources/rbs/components/collection/SelectResults.js
+++ src/main/resources/META-INF/resources/rbs/components/collection/SelectResults.js
@@ -2,6 +2,12 @@ define([ "react", "underscore", "../mixins/Collection" ],
   function (React, _, collection) {
     "use strict";
 
+
+    var MAX_HEIGHT_EM = 20;
+    var MIN_HEIGHT_EM = 3;
+    var BOTTOM_BUFFER = 1;
+    var ONE_EM = 16;
+
     // renders a collection of results as the results of a select dropdown
     // fires an onSelect(model) event for when an option is clicked
     return _.rf({
@@ -22,7 +28,8 @@ define([ "react", "underscore", "../mixins/Collection" ],
 
       getInitialState: function () {
         return {
-          hilite: 0
+          hilite: 0,
+          maxHeight: null
         };
       },
 
@@ -34,16 +41,20 @@ define([ "react", "underscore", "../mixins/Collection" ],
         this.setHilite(this.state.hilite - 1);
       },
       pageDown: function () {
-        this.setHilite(this.state.hilite + 10);
+        this.setHilite(this.state.hilite + this.getPageSize());
       },
       pageUp: function () {
-        this.setHilite(this.state.hilite - 10);
+        this.setHilite(this.state.hilite - this.getPageSize());
       },
       home: function () {
         this.setHilite(0);
       },
       end: function () {
         this.setHilite(this.state.collection.length - 1);
+      },
+
+      getPageSize: function () {
+        return Math.max(Math.floor(this.state.maxHeight / 3), 1)
       },
 
       setHilite: function (newHilite) {
@@ -93,8 +104,48 @@ define([ "react", "underscore", "../mixins/Collection" ],
         return null;
       },
 
+      componentDidMount: function () {
+        this.calculateMaxHeight = _.debounce(_.bind(this._calculateMaxHeight, this), 20);
+        this.calculateMaxHeight();
+        $(window).on("resize", this.calculateMaxHeight);
+      },
+
+      componentWillUnmount: function () {
+        $(window).off("resize", this.calculateMaxHeight);
+      },
+
+      _calculateMaxHeight: function () {
+        if (this.isMounted()) {
+          var wind = $(window);
+          var resultsDiv = $(React.findDOMNode(this.refs.results));
+          var windowScrollTop = wind.scrollTop();
+          // how far from the top of the window the select results are
+          var divDistanceFromTop = resultsDiv.offset().top - windowScrollTop;
+          // how tall the window is
+          var windowHeight = wind.height();
+
+          var maxHeightInEm = Math.round((windowHeight - divDistanceFromTop) / ONE_EM) - BOTTOM_BUFFER;
+          maxHeightInEm = Math.max(Math.min(MAX_HEIGHT_EM, maxHeightInEm), MIN_HEIGHT_EM);
+
+          if (this.state.maxHeight !== maxHeightInEm) {
+            this.setState({
+              maxHeight: maxHeightInEm
+            });
+          }
+        }
+      },
+
       componentDidUpdate: function () {
         this.setHilite(this.state.hilite);
+      },
+
+      calculateStyle: function () {
+        if (this.state.maxHeight !== null) {
+          return {
+            maxHeight: this.state.maxHeight + "em"
+          };
+        }
+        return {};
       },
 
       render: function () {
@@ -125,9 +176,12 @@ define([ "react", "underscore", "../mixins/Collection" ],
           }, this.props.emptyMessage));
         }
 
+        var style = this.calculateStyle();
+
         // put all the results in an absolutely positioned div under the search box
         return React.DOM.div(_.extend({}, this.props, {
           className: "react-select-search-results " + (this.props.className || ""),
+          style: style,
           ref: "results"
         }), results);
       }
