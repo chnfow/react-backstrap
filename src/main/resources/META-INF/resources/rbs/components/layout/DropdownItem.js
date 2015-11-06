8a95e3f898422801f9cce1ac31c788d4491b4f87
Moody Salem <moody.salem@gmail.com>
2015-11-06 00:15:22 -0600

    improve dropdown item to allow href

--- src/main/resources/META-INF/resources/rbs/components/layout/DropdownItem.js
+++ src/main/resources/META-INF/resources/rbs/components/layout/DropdownItem.js
@@ -5,40 +5,39 @@ define([ "react", "underscore", "./Icon", "util" ],
   function (React, _, icon, util) {
     "use strict";
     var rpt = React.PropTypes;
+    var d = React.DOM;
     return util.rf({
       displayName: "Dropdown Item",
 
       mixins: [ React.addons.PureRenderMixin ],
 
       propTypes: {
+        caption: rpt.node.isRequired,
         icon: rpt.node,
-        caption: rpt.node,
+        href: rpt.string,
         onClick: rpt.func
       },
 
       getDefaultProps: function () {
         return {
-          onClick: _.noop
+          href: "#",
+          icon: null
         };
       },
 
 
       handleClick: function (e) {
-        this.props.onClick(e);
-      },
-
-      doNothing: function (e) {
-        e.preventDefault();
+        if (typeof this.props.onClick === "function") {
+          this.props.onClick(e);
+        }
       },
 
       getIcon: function () {
         if (typeof this.props.icon === "string") {
           return icon({ key: "i", name: this.props.icon });
-        }
-        if (typeof this.props.icon !== "undefined") {
+        } else {
           return this.props.icon;
         }
-        return null;
       },
 
       render: function () {
@@ -46,9 +45,8 @@ define([ "react", "underscore", "./Icon", "util" ],
         if (this.props.children) {
           children = children.concat(this.props.children);
         }
-        return React.DOM.li({ onClick: this.handleClick }, React.DOM.a({
-          onClick: this.doNothing,
-          href: "#"
+        return d.li({ onClick: this.handleClick }, d.a({
+          href: this.props.href
         }, children));
       }
     });
