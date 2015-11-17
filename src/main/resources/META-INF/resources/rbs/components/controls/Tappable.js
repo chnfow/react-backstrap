453173ce0b1660dd05595a13d656e820775c4b8e
Moody Salem <moody@fastmodeltechnologies.com>
2015-11-17 12:36:42 -0600

    remove shouldcomponentupdate because it causes trouble with re-rendering when the tappable is the top level element

--- src/main/resources/META-INF/resources/rbs/components/controls/Tappable.js
+++ src/main/resources/META-INF/resources/rbs/components/controls/Tappable.js
@@ -21,10 +21,6 @@ define([ "react", "jquery", "underscore", "util" ], function (React, $, _, util)
       };
     },
 
-    shouldComponentUpdate: function (nextProps) {
-      return this.props.children !== nextProps.children;
-    },
-
     getInitialState: function () {
       return {
         touchId: null,
